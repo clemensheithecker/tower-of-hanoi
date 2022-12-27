e89d0b10c7a6a6d3e5a3a314d74a32b00f2c2adf
Clemens Stefan Heithecker <clemens@heithecker.dev>
2022-12-27 23:32:09 +0100

    Add unit test for `getBackgroundColorClass`

--- src/utilities/getBackgroundColorClass.ts
+++ src/utilities/getBackgroundColorClass.ts
@@ -5,6 +5,14 @@ const getBackgroundColorClass = ({
   initialPosition: number;
   backgroundColorClasses: string[];
 }): string => {
+  if (initialPosition < 0) {
+    throw new Error("The initial position must be at least 0.");
+  }
+
+  if (backgroundColorClasses.length === 0) {
+    throw new Error("The background color classes array is empty.");
+  }
+
   const index = initialPosition % backgroundColorClasses.length;
 
   return backgroundColorClasses[index];
